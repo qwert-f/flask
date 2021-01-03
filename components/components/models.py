0b16c28cf85d72f622b96a611b2a651edc378696
滇弦 <ogicout@loicout.cf>
2021-01-03 21:47:34 +0800

    correct pwd

--- components/components/models.py
+++ components/components/models.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 
 
 class sql():
-    engine = create_engine('mysql+pymysql://root:sql1@127.0.0.1:3306/daily')
-    # engine = create_engine('mysql+pymysql://root:sql1@127.0.0.1:3306/flask')
+    # engine = create_engine('mysql+pymysql://root:sql1@127.0.0.1:3306/daily')
+    engine = create_engine('mysql+pymysql://root:aws@ubuntu@127.0.0.1:3306/flask')
     cur = engine.execute('select name from users')
 
 
